45f98dd76622c436f564c660716584bfb7da6475
Hosain <hosainandh@gmail.com>
2025-04-10 12:33:55 +0600

    Resigter user

---
+++ server/routes/userRouter.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { register, verifyOTP } from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/otp-verification", verifyOTP);
+
+export default router;
